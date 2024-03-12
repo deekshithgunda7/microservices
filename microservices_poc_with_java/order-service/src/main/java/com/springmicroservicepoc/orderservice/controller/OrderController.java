2fb79c82c420a07787d898ebef87534bee5d9cf9
deekshithgunda7 <deekshith.gunda7@gmail.com>
2024-03-12 00:44:20 -0500

    added resilence4j(Circuit Breaking) in between order and inventory service

--- microservices_poc_with_java/order-service/src/main/java/com/springmicroservicepoc/orderservice/controller/OrderController.java
+++ microservices_poc_with_java/order-service/src/main/java/com/springmicroservicepoc/orderservice/controller/OrderController.java
@@ -3,22 +3,36 @@ package com.springmicroservicepoc.orderservice.controller;
 
 import com.springmicroservicepoc.orderservice.dto.OrderRequest;
 import com.springmicroservicepoc.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order")
+@Slf4j
 public class OrderController {
 
 
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
 
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully ";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
